5bd8a951006c50af492e809c0879740bdf0065fa
Patrick-web <pntxall100@gmail.com>
2022-07-30 04:23:38 +0300

    working rust

--- plugins/init.lua
+++ plugins/init.lua
@@ -13,19 +13,26 @@ return {
    ["simrat39/symbols-outline.nvim"] = {},
    ["nvim-telescope/telescope-ui-select.nvim"] = {},
    ["windwp/nvim-spectre"] = {},
-   ["williamboman/mason.nvim"] = {},
-  --  ["williamboman/mason-lspconfig.nvim"] = {
-  --    config = function()
-  --     require("mason-lspconfig").setup({
-  --       ensure_installed = { "sumneko_lua", "rust_analyzer" }
-  --     })
-  --   end
-  --  },
-  --  ["neovim/nvim-lspconfig"] = {
-  --   config = function()
-  --     -- require "plugins.configs.lspconfig"
-  --     require "custom.plugins.lspconfig"
-  --   end,
-  -- },
-  -- ["williamboman/nvim-lsp-installer"] = {}
+   ["neovim/nvim-lspconfig"] = {
+    config = function()
+      -- require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  ["williamboman/nvim-lsp-installer"] = {
+    ensure_installed = {
+        "sumneko_lua",
+  "rust_analyzer",
+  "tsserver",
+  "jdtls",
+  "jsonls",
+  "volar",
+  "cssls",
+  "tailwindcss",
+  "gopls",
+  "emmet_ls",
+  "eslint",
+  "clangd",
+    }
+  }
 }
